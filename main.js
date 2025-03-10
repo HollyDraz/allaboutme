4632d3b602f56ee017241df84b763dfbe8990e8e
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-03-10 10:26:33 -0500

    added new function

--- main.js
+++
@@ -1,7 +0,0 @@
-
-function addRecommendation() {
-    const newRecommendation = document.createElement("div");
-    newRecommendation.classList.add("recommendation");
-    newRecommendation.innerHTML = "<p>'Holly is a dedicated and hardworking individual!' - New User</p>";
-    document.getElementById("recommendations").appendChild(newRecommendation);
-}
